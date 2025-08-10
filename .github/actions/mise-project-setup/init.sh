c24d66c1b230ca99c4ea5166db8c97788bc4082a
Niko Böckerman <git.585wj@slmail.me>
2025-08-10 10:27:49 +0300

    Use one mise cache per OS for all directories

--- .github/actions/mise-project-setup/init.sh
+++ .github/actions/mise-project-setup/init.sh
@@ -19,6 +19,15 @@ case "$CACHE_MODE" in
     ;;
 esac
 
+if [ "$CACHE_MODE" = "prepare" ]; then
+  if [ -n "${DIRECTORY}" ]; then
+    echo "::error Directory must be empty for prepare mode"
+    exit 1
+  fi
+
+  DIRECTORY="."
+fi
+
 if [ -z "${DIRECTORY}" ]; then
   echo "::error Empty DIRECTORY is not supported"
   exit 1
@@ -28,37 +37,6 @@ elif [ ! -d "$DIRECTORY" ]; then
 elif [ ! -f "$DIRECTORY/mise.toml" ]; then
   echo "::error Directory does not contain mise.toml: $DIRECTORY"
   exit 1
-fi
-
-if [ "${DIRECTORY}" = "." ]; then
-  dir_key=""
-else
-  dir_key="-${DIRECTORY/\//-}"
-fi
-
-cache_key_prefix="mise${dir_key}-${MISE_VERSION}-${RUNNER_OS}"
-cache_key_mise_root=$(sha256sum "mise.toml" | cut -d ' ' -f 1)
-cache_key_mise_directory=""
-cache_key_mise_aoc_main=""
-if [ "${DIRECTORY}" != "." ]; then
-  cache_key_mise_directory=$(sha256sum "${DIRECTORY}"/mise.toml | cut -d ' ' -f 1)
-  if [ "${DIRECTORY}" != "aoc-main" ]; then
-    cache_key_mise_aoc_main=$(sha256sum aoc-main/mise.toml | cut -d ' ' -f 1)
-  fi
-fi
-
-cache_key="${cache_key_prefix}-${cache_key_mise_root}"
-if [ -n "${cache_key_mise_directory}" ]; then
-  cache_key="${cache_key}-${cache_key_mise_directory}"
-  if [ -n "${cache_key_mise_aoc_main}" ]; then
-    cache_key="${cache_key}-${cache_key_mise_aoc_main}"
-  fi
-fi
-
-cache_key_root=""
-if [ "${DIRECTORY}" != "." ]; then
-  cache_key_root_prefix="mise-${MISE_VERSION}-${RUNNER_OS}"
-  cache_key_root="${cache_key_root_prefix}-${cache_key_mise_root}"
 fi
 
 mise_data_dir="${RUNNER_TEMP}${SEP}aoc-mise-data"
@@ -78,32 +56,29 @@ cargo_home="${RUNNER_TEMP}${SEP}aoc-cargo-home"
 
 # Set outputs
 {
-  echo "cache-key=${cache_key}"
   echo "mise-version=${MISE_VERSION}"
 
   # Install directories
   echo "mise-install-directories<<ENDDIRS"
-  echo "${DIRECTORY}"
-  if [ "${DIRECTORY}" != "." ] && [ "${DIRECTORY}" != "aoc-main" ]; then
+  echo "."
+  if [ "$CACHE_MODE" = "prepare" ]; then
     echo "aoc-main"
+    # Use of mise tools in CI on different OSes for solvers:
+    #   - C++: Ubuntu
+    #   - Python: Ubuntu
+    #   - Rust: Ubuntu, Windows and macOS
+    if [ "$RUNNER_OS" = "Linux" ]; then
+      echo solvers/cpp
+      echo solvers/python
+    fi
+    echo solvers/rust
+  else
+    if [ "$DIRECTORY" != "."  ] && [ "$DIRECTORY" != "aoc-main" ]; then
+      echo "aoc-main"
+    fi
+    echo "${DIRECTORY}"
   fi
   echo "ENDDIRS"
-
-  # Cache restore keys
-  echo "cache-restore-keys<<ENDKEYS"
-  if [ "$CACHE_MODE" = "prepare" ]; then
-    if [ -n "${cache_key_mise_directory}" ]; then
-      if [ -n "${cache_key_mise_aoc_main}" ]; then
-        echo "${cache_key_prefix}-${cache_key_mise_root}-${cache_key_mise_directory}-"
-      fi
-      echo "${cache_key_prefix}-${cache_key_mise_root}-"
-    fi
-    echo "${cache_key_prefix}-"
-    if [ -n "${cache_key_root}" ]; then
-      echo "${cache_key_root}"
-    fi
-  fi
-  echo "ENDKEYS"
 
   # Mise cache paths
   echo "cache-paths<<ENDPATHS"
