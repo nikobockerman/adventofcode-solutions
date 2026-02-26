050ac5874113c91a73b1c13b37147ad599ef913f
Niko Böckerman <git.585wj@slmail.me>
2026-02-26 23:32:07 +0200

    Use homebrew keg paths to ensure correct version (#442)

--- .github/actions/homebrew-packages-setup/init.sh
+++ .github/actions/homebrew-packages-setup/init.sh
@@ -49,6 +49,10 @@ echo "::group::Environment variable changes"
 {
   echo "HOMEBREW_DISPLAY_INSTALL_TIMES=1"
   echo "HOMEBREW_NO_AUTO_UPDATE=1"
+
+  # Set GCC and LLVM major versions
+  echo "GCC_MAJOR_VERSION=${GCC_MAJOR_VERSION}"
+  echo "LLVM_MAJOR_VERSION=${LLVM_MAJOR_VERSION}"
 } | tee -a "${GITHUB_ENV}"
 echo "::endgroup::"
 
